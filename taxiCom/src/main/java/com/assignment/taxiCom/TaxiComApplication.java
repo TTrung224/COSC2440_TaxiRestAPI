e151ac443272e2c7333ffd9f00e8392e004dfaf0
Trung Tran <s3891724@rmit.edu.vn>
2022-04-26 20:03:00 +0700

    create project

---
+++ taxiCom/src/main/java/com/assignment/taxiCom/TaxiComApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.taxiCom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiComApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxiComApplication.class, args);
+	}
+
+}
---
+++ taxiCom/src/test/java/com/assignment/taxiCom/TaxiComApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.taxiCom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiComApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

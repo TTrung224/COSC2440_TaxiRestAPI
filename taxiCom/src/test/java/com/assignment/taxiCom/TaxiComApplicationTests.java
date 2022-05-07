11fdbf0def393e96852fe270b9a2151b39ef708b
Trung Tran <s3891724@rmit.edu.vn>
2022-05-07 10:46:41 +0700

    add some first unit testing for booking

--- taxiCom/src/test/java/com/assignment/taxiCom/TaxiComApplicationTests.java
+++ taxiCom/src/test/java/com/assignment/taxiCom/TaxiComApplicationTests.java
@@ -1,13 +1,83 @@
 package com.assignment.taxiCom;
 
+
+import com.assignment.taxiCom.entity.Booking;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-class TaxiComApplicationTests {
+@AutoConfigureMockMvc
+public class TaxiComApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+//	Positive Tests
+	@Test
+	public void AddBookingTest() throws Exception {
+		RequestBuilder request = MockMvcRequestBuilders
+				.post("/booking")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{\"distance\": 1000,\n" +
+						"    \"startingLocation\": \"HCM\",\n" +
+						"    \"endLocation\": \"DN\",\n" +
+						"    \"pickUpTime\": \"2022-02-01T08:00:00+07:00\",\n" +
+						"    \"dropOffTime\": \"2022-03-01T08:00:00+07:00\"}");
+		MvcResult result = mockMvc.perform(request)
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andReturn();
+	}
 
 	@Test
-	void contextLoads() {
+	public void GetAllBookingTest() throws Exception {
+		RequestBuilder request = MockMvcRequestBuilders.get("/booking");
+		MvcResult result = mockMvc.perform(request)
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().contentType("application/json"))
+				.andReturn();
 	}
+
+//	Negative Tests
+	@Test
+	public void AddBookingWithoutBodyTest() throws Exception {
+		RequestBuilder request = MockMvcRequestBuilders.post("/booking");
+		MvcResult result = mockMvc.perform(request)
+				.andDo(print())
+				.andExpect(status().isBadRequest())
+				.andReturn();
+	}
+
+	@Test
+	public void AddBookingTestWithWrongZonedDateTimeFormat() throws Exception {
+		RequestBuilder request = MockMvcRequestBuilders
+				.post("/booking")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{\"distance\": 1000,\n" +
+						"    \"startingLocation\": \"HCM\",\n" +
+						"    \"endLocation\": \"DN\",\n" +
+						"    \"pickUpTime\": \"2022-02-01T08:00:00\",\n" +
+						"    \"dropOffTime\": \"2022-03-01T08:00:00\"}");
+		MvcResult result = mockMvc.perform(request)
+				.andDo(print())
+				.andExpect(status().isBadRequest())
+				.andReturn();
+	}
+
 
 }
